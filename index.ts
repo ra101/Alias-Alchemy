7951b5195dfb5ce0e53255d94dad46777a193349
ra101 <agarwal.parth.101@gmail.com>
2023-12-02 03:58:30 +0530

    Update Alchemy Circle

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
-import aliasJSON from 'https://raw.githubusercontent.com/ra101/alias/core/alias.json' assert { type: "json" };
+import tools from './tools.json' assert { type: "json" };
+
+
+const aliasData = {}
+for await (const file of Deno.readDir()) {
+  if (file.name.includes('.alias.sh')){
+    aliasData[file.name.split('.')[1]] = await Deno.readFile(file.name);
+  }
+}
+const alias_categories = Object.keys(aliasData)
 
 
 async function requestHandler (request: Request): Response {
   const userAgent = request.headers.get('user-agent')
   if (userAgent && isCliRequest(userAgent)){
-      return aliasFileResponse(request);
+      return await aliasFileResponse(request);
   }
-  const webpage = await fetch('https://ra101.dev/alias')
+
+  const webpage = await fetch('https://ra101.dev/alias-alchemy')
   let webpageHTML = await webpage.text();
   webpageHTML = webpageHTML.replaceAll("\"\/", "\"https://ra101.dev/")  
   const headers = new Headers(webpage.headers);
@@ -17,7 +27,7 @@ async function requestHandler (request: Request): Response {
 
 
 function isCliRequest(userAgent: string): Boolean{
-  for (const tool of aliasJSON.tools) {
+  for (const tool of tools) {
     if (userAgent.toLowerCase().includes(tool)) {
       return true;
     }
@@ -26,50 +36,59 @@ function isCliRequest(userAgent: string): Boolean{
 }
 
 
-function aliasFileResponse(request: Request){
+
+
+async function aliasFileResponse(request: Request){
     let query = (new URL(request.url)).searchParams.get('q') || '';
     query = query.toLowerCase().replaceAll(' ', '').replace(/(^,+)|(,+$)/g, '')
     
-    const all_alias_cats = Object.keys(aliasJSON.category)
-    if (query == '') {
-      const alias_cat = all_alias_cats
-    }
-    else {
-      const alias_cat = query.split(',')
+    let q_alias_cat = [...alias_categories]
 
-      for (const cat of alias_cat) {
-        if (! all_alias_cats.includes(cat)) {
+    if (query != '') {
+      q_alias_cat = query.split(',')
+
+      for (const cat of q_alias_cat) {
+        if (! alias_categories.includes(cat)) {
           return new Response(
             `Invalid Alias Category '${cat}'! Valid Categories` + 
-            ` are [ ,${all_alias_cats}]`, { status: 400 }
+            ` are [ ,${alias_categories}]`, { status: 400 }
           );
         }
       }
     }
 
-    aliasFile = createAliasFile(alias_cat)
+    const aliasFile = createAliasFile(q_alias_cat)
+    const headers = new Headers();
+    headers.set('Content-Type', 'text/plain');
+    headers.set('Content-Disposition', 'attachment; filename="alias.sh"')
+    const init: ResponseInit = {headers};
+
+    return new Response(aliasFile, init)
+  }
 
 
-    return new Response(null, { status: 200 });
-}
+function createAliasFile(q_alias_cat: Array<string>){
+  let aliasFileSize = 0
+  const newline = new TextEncoder().encode('\n')
+  
+  for (const cat of q_alias_cat) {
+    aliasFileSize += (aliasData[cat].length + newline.length)
+  }
+  aliasFileSize -= newline.length
 
+  let sizeOffset = 0
+  const aliasFile = new Uint8Array(aliasFileSize)
+  const lastCategory = q_alias_cat[q_alias_cat.length - 1] 
 
-function createAliasFile(alias_cat: Array<string>){
-  for (const cat of alias_cat) {
-    const catObj = aliasJSON.category[cat]
-    for (const command in catObj){
-      switch (command['type']) {
-        case 'alias':
-          day = "Sunday";
-          break;
-        case 'cmd':
-          day = "Monday";
-          break;
-        case 'env':
-           day = "Tuesday";
-      }
+  for (const cat of q_alias_cat) {
+    aliasFile.set(aliasData[cat], sizeOffset)
+    if (cat != lastCategory){
+      sizeOffset += aliasData[cat].length
+      aliasFile.set(newline, sizeOffset)
+      sizeOffset += newline.length
     }
   }
+  return aliasFile
 }
 
 
